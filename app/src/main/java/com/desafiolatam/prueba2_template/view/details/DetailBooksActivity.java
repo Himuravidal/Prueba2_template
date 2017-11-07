d909db20083fb9ff727eda5e0d2e42738e540af7
cutiko <cutiko@gmail.com>
2017-11-07 11:58:55 -0300

    FIX: book details is more pedagogical

--- app/src/main/java/com/desafiolatam/prueba2_template/view/details/DetailBooksActivity.java
+++ app/src/main/java/com/desafiolatam/prueba2_template/view/details/DetailBooksActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
 import com.desafiolatam.prueba2_template.R;
 import com.desafiolatam.prueba2_template.models.Book;
@@ -13,8 +12,7 @@ public class DetailBooksActivity extends AppCompatActivity {
 
 
     private Book book;
-    private EditText descriptionBook;
-    private TextView authorTV;
+    private EditText review, author;
     private SeekBar progressBar;
 
     @Override
@@ -22,10 +20,17 @@ public class DetailBooksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-       //TODO :debes recibir los datos del intent mostrar los textos, y guardar la data cuando el usuario cambie de pantalla.
+        //TODO get the intent from the previous activity, using that id find the book in the database
+        //TODO get every view, look at the layout and the field variables
+        //TODO set the attributes of the book to the views (author, review, progress), don't forget to validate the attribute in the book is not null
+        //TODO don't forget to set the activity title as the book title
 
     }
 
+
+
+
+    //TODO remember you have to save without the user noticing it, it has to be flawless, override the activity life cycle for that
 
 
 
