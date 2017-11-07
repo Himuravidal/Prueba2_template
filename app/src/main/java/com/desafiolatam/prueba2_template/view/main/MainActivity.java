b4ad74d60c2f36cef2151977f7dc34bda6502682
cutiko <cutiko@gmail.com>
2017-11-07 11:27:27 -0300

    REMOVE: callback pattern is not a content yet

--- app/src/main/java/com/desafiolatam/prueba2_template/view/main/MainActivity.java
+++ app/src/main/java/com/desafiolatam/prueba2_template/view/main/MainActivity.java
@@ -1,27 +1,20 @@
 package com.desafiolatam.prueba2_template.view.main;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.desafiolatam.prueba2_template.R;
-import com.desafiolatam.prueba2_template.models.Book;
 import com.desafiolatam.prueba2_template.view.main.bookList.BookListFragment;
 
-public class MainActivity extends AppCompatActivity implements CreateCallback {
+public class MainActivity extends AppCompatActivity {
 
     private Dialog dialog;
     private BookListFragment bookListFragment;
@@ -41,13 +34,9 @@ public class MainActivity extends AppCompatActivity implements CreateCallback {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 EditText bookInput = dialog.findViewById(R.id.bookInputET);
                 bookInput.setText("");
                 dialog.show();
-                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputMethodManager.showSoftInput(getCurrentFocus(), InputMethodManager.SHOW_FORCED);
-
             }
         });
     }
@@ -70,46 +59,7 @@ public class MainActivity extends AppCompatActivity implements CreateCallback {
             }
         });
 
-        bookInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if (i == EditorInfo.IME_ACTION_NEXT || i == EditorInfo.IME_ACTION_DONE) {
-
-                    String bookName = bookInput.getText().toString();
-                    createdBooks(bookName);
-
-                    return true;
-                }
-
-                return false;
-            }
-        });
 
 
-    }
-
-
-    private void createdBooks(String name) {
-
-        BookValidation bookValidation = new BookValidation(MainActivity.this);
-        bookValidation.init(name);
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
-        dialog.dismiss();
-
-
-    }
-
-    @Override
-    public void success(Book book) {
-
-        bookListFragment.addBook(book);
-
-    }
-
-    @Override
-    public void fail() {
-
-        Toast.makeText(this, "Ingresa un Libro", Toast.LENGTH_SHORT).show();
     }
 }
